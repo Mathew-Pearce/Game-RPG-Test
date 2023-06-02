2fd5bdb2e10359e7205a61dbb81b7b00fefa9f59
Mathew <matpearce1984@live.co.uk>
2023-06-02 14:11:39 +0100

    Basic rigid body movement and jump added. Jumps endlessly.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rigidBoddy2D;
+    private float xInput;
+    public float walkSpeed = 5f;
+    public float jumpForce = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump");
+        }
+        xInput = Input.GetAxisRaw("Horizontal");
+        rigidBoddy2D.velocity = new Vector2(xInput * walkSpeed, rigidBoddy2D.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidBoddy2D.velocity = new Vector2(rigidBoddy2D.velocity.x, jumpForce);
+        }
     }
 }
