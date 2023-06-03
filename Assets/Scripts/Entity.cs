983475b3ca7ae4311c3b8d7518baa59b6580dd78
Mathew <matpearce1984@live.co.uk>
2023-06-03 17:28:59 +0100

    Major code refactor with variable name changes. Inheritance between player and entity scripts to set up for making enemies. Add ground check obj to player. which meant using on drwa gizmos again as was no longer selecting the player.....

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Rigidbody2D rb;
+    protected Animator anim;
+
+    [Header("Ground collision Params")]
+    [SerializeField] protected float groundCheckDistance = 0.2f;
+    [SerializeField] protected LayerMask groundMask;
+    [SerializeField] protected Transform groundCheck;
+    
+    protected bool isGrounded;
+
+    protected int faceDir = 1;
+    protected bool isFacingRight = true;
+
+    protected virtual void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    protected virtual void Update()
     {
-        
+        CheckIfGrounded();
+    }
+
+    protected virtual void CheckIfGrounded()
+    {
+        isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundMask);
+    }
+
+    protected virtual void FlipSprite()
+    {
+        faceDir = faceDir * -1;
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
     [Header("Player control Params")]
     [SerializeField] float walkSpeed = 5f;
@@ -21,29 +21,17 @@ public class Player : MonoBehaviour
     [SerializeField] float comboTime = 0.3f;
     private bool isAttacking;
     private int comboCounter;
-  
 
-
-    [Header("Ground collision Params")]
-    [SerializeField] float groundCheckDistance = 0.2f;
-    [SerializeField] LayerMask groundMask;
-
-    private Animator animator;
-    private Rigidbody2D rigidBoddy2D;
     private float xInput;
-    private int faceDir = 1;
-    private bool isFacingRight = true;
-    private bool isGrounded;
-
-    void Start()
+    protected override void Start()
     {
-        rigidBoddy2D = GetComponent<Rigidbody2D>();
-        animator = GetComponentInChildren<Animator>();
+        base.Start();
     }
 
-  
-    void Update()
+    protected override void Update()
     {
+        base.Update();
+
         HandleInput();
         HandleMovement();
         CheckIfGrounded();
@@ -88,7 +76,7 @@ public class Player : MonoBehaviour
     {
         if (isGrounded)
         {
-            rigidBoddy2D.velocity = new Vector2(rigidBoddy2D.velocity.x, jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
     }
@@ -97,15 +85,15 @@ public class Player : MonoBehaviour
     {
         if (isAttacking)
         {
-            rigidBoddy2D.velocity = new Vector2(0, 0);
+            rb.velocity = new Vector2(0, 0);
         }
         else if(dashTimer > 0)
         {
-            rigidBoddy2D.velocity = new Vector2(faceDir * dashSpeed, 0);
+            rb.velocity = new Vector2(faceDir * dashSpeed, 0);
         }
         else
         {
-            rigidBoddy2D.velocity = new Vector2(xInput * walkSpeed, rigidBoddy2D.velocity.y);
+            rb.velocity = new Vector2(xInput * walkSpeed, rb.velocity.y);
         }
 
         
@@ -114,28 +102,22 @@ public class Player : MonoBehaviour
 
     private void HandleAnimation()
     {
-        bool isMoving = rigidBoddy2D.velocity.x != 0;
-        animator.SetBool("isMoving", isMoving);
-        animator.SetFloat("yVelocity", rigidBoddy2D.velocity.y);
-        animator.SetBool("isGrounded", isGrounded);
-        animator.SetBool("Dashing", dashTimer > 0);
-        animator.SetBool("isAttacking", isAttacking);
-        animator.SetInteger("ComboCounter", comboCounter);
+        bool isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("Dashing", dashTimer > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("ComboCounter", comboCounter);
 
     }
 
-    private void FlipSprite()
-    {
-        faceDir = faceDir * -1;
-        isFacingRight = !isFacingRight;
-        transform.Rotate(0, 180, 0);
-    }
 
     private void FlipController()
     {
-        if(rigidBoddy2D.velocity.x > 0 && !isFacingRight) 
+        if(rb.velocity.x > 0 && !isFacingRight) 
             FlipSprite(); 
-        else if (rigidBoddy2D.velocity.x < 0 && isFacingRight)
+        else if (rb.velocity.x < 0 && isFacingRight)
             FlipSprite();
     }
 
@@ -144,16 +126,6 @@ public class Player : MonoBehaviour
         dashTimer -= Time.deltaTime;
         dashCooldownTimer -= Time.deltaTime;
         comboTimeWindow -= Time.deltaTime;
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
-    }
-
-    private void CheckIfGrounded()
-    {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundMask);
     }
 
     private void StartAttack()
